cdef27d8d53d9b399d586144c5e6aa5dfdcf29af
Antoine <antoinum@gmail.com>
2019-10-14 20:55:51 +0200

    mise a jour :wq
    h

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useContext } from 'react';
 import {BrowserRouter as Router, __RouterContext, Switch, Route} from 'react-router-dom'
 import {Layout} from 'antd';
 import  ZIndex  from 'react-z-index'
-import AboutPage from './Pages/About/aboutPage'
-import {useTransition, animated} from 'react-spring'
+
+import AboutPage from './Pages/About'
+import PortfolioPage from './Pages/Portfolio';
+
 import './App.css';
-import PortfolioPage from './Pages/Portfolio/PortfolioPage';
 
 const App = () => {
   const {Footer } = Layout;
@@ -28,7 +29,7 @@ const App = () => {
           <Switch>
               {/* <Route path="/home"  component={props => <Homepage />} /> */}
               <Route exact path="/" component={AboutPage} />
-              <Route exact path="/portfolio" component={props => <PortfolioPage />} />
+              <Route exact path="/portfolio" component={PortfolioPage} />
           </Switch>
         </ZIndex>
           
