2f7005a80b5f5fa62faa7ada4a80194c48b77cef
Antoine <antoinum@gmail.com>
2019-10-17 01:10:09 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7747b8533e092924ebcaeb50afda345.js"
+  "/precache-manifest.cd240707920126ab696383416bade267.js"
 );
 
 self.addEventListener('message', (event) => {
